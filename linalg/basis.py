cb03c27b771a1fd997b42fe4f3cb59a7aa1a0038
HammerSeb <sebastian@hammer-mail.com>
2023-08-17 12:33:03 -0400

    linear independence check

--- linalg/basis.py
+++ linalg/basis.py
@@ -1,8 +1,26 @@
 import numpy as np
 from numpy.linalg import det
 
+class LinearAlgebraError(Exception):
+    pass
 
-class basis():
+def check_linear_independence():
+    def check_accepts(function):
+        def new_function(*args, **kwargs):
+            basis = np.array([*args[1:]])
+            if det(basis)==0:
+                raise LinearAlgebraError("vectors are not linear independent and cannot form a basis")
+
+            return function(*args, **kwargs)
+
+        new_function.__name__ = function.__name__
+
+        return new_function
+    return check_accepts
+
+
+class basis:
+    @check_linear_independence()
     def __init__(self, v1, v2, v3):
         """
         Generates basis from three linear independent vectors v1, v2, v3
@@ -10,33 +28,12 @@ class basis():
         :param v2: list, tuple or nd.array with three entries as coordinated v21, v22, v23
         :param v3: list, tuple or nd.array with three entries as coordinated v31, v32, v33
         """
-        _vectors = [v1, v2, v3]
-        for _v in _vectors:
-            if _v is type(np.ndarray):
-                pass
-            else:
-                _v = np.array(_v)
-
-        self.v1, self.v2, self.v3 = v1, v2, v3
-
-        try:
-            if self.__check_linear_independece == False:
-                raise LinearAlgebraError
-        except LinearAlgebraError:
-            print("vectors are not linear independent and cannot form a basis")
+        self.basis = np.array([v1, v2, v3])
 
 
 
 
-    def __check_linear_independece(self):
-        """
-        checks if vectors v1, v2, v3 are linear independent by calculating the determinant
-        """
-        M = np.array([self.v1,self.v2,self.v3])
-        return det(M) == 0
 
 
 
 
-class LinearAlgebraError(Exception):
-    pass
