b7fc534296967c5e862929d17519f0e461633bb5
HammerSeb <sebastian@hammer-mail.com>
2023-08-23 09:58:45 -0400

    basis transformation cleanup

--- linalg/transformations.py
+++ linalg/transformations.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numpy.linalg import inv
 
+from .basis import basis, vector, standard_basis
+
 class basis_transformation:
     def __init__(self, basis1, basis2):
         """
@@ -8,6 +10,8 @@ class basis_transformation:
         :param start_basis:
         :param target_basis:
         """
+        self.basis1 = basis1
+        self.basis2 = basis2
         self.t_matrix = np.dot(basis1.basis, inv(basis2.basis))
         self.invt_matrix = inv(self.t_matrix)
 
@@ -18,7 +22,8 @@ class basis_transformation:
         :param v: nd.array of shape (3,)
         :return: nd.array of shape (3,)
         """
-        return np.dot(v,self.t_matrix)
+        _v = np.dot(v.vector, self.t_matrix)
+        return vector(_v, self.basis2)
 
     def inv_transform(self, v):
         """
@@ -26,4 +31,5 @@ class basis_transformation:
         :param v: nd.array of shape (3,)
         :return: nd.array of shape (3,)
         """
-        return np.dot(v, self.invt_matrix)
+        _v = np.dot(v.vector, self.invt_matrix)
+        return vector(_v, self.basis1)
--- linalg_test.py
+++ linalg_test.py
@@ -1,4 +1,5 @@
-from linalg.basis import vector, basis
+from linalg.basis import vector, basis, standard_basis
+from linalg.transformations import basis_transformation
 
 v1 = [1, 0, 0]
 v2 = [0, 1, -1]
@@ -6,6 +7,15 @@ v3 = [1, 0, 1]
 
 b = basis(v1,v2,v3)
 
+trans = basis_transformation(b, standard_basis)
+
 a = vector([1,1,0], b)
 c = vector([0,1,1], b)
 
+a2 = trans.transform(a)
+x = vector([1,1,-1], standard_basis)
+a3 = trans.inv_transform(x)
+
+print(a.global_coord)
+print(a2.vector)
+print(a3.vector, a3.global_coord)
