a23c0142854fdb51a39013cdfba57795b9e70df6
HammerSeb <sebastian@hammer-mail.com>
2023-09-21 16:25:56 -0400

    pyqt integration

--- drawing_tests.py
+++ drawing_tests.py
@@ -3,22 +3,25 @@ import pyqtgraph as pg
 import pyqtgraph.opengl as gl
 from pyqtgraph import functions as fn
 from pyqtgraph.Qt import QtCore
+from linalg.basis import basis, vector
+from draw.draw import make_figure, GLPoints, draw_basis, draw_frame
+from draw.cells import draw_cell, draw_supercell
+from cell.contents import super_cell
+from cell.generate import cell_from_cif
+from cell.tools import move
 
-from draw.draw import make_figure, GLPoints
 
-
+cll = cell_from_cif("testdata/erk.cif")
+cll.atoms_to_molecule()
+spcll = super_cell(cll, (5,3,1))
+move(spcll.molecules[7],vector([0.5,0,0], spcll.lattice))
 
 w = make_figure()
 
+# _ = draw_frame(w, cll.lattice)
+# _ = draw_basis(w, cll.lattice)
+test = draw_supercell(w, spcll, lw=1)
 
-pos = np.array([[0,0,0], [1,0,0], [0,1,0], [1,1,1]])
-color = np.array([ [1,1,1,1] for i in range(4) ])
-size = np.array([0.4 for i in range(4)])
-
-pts = GLPoints(pos=pos,color=color,size=size)
-w.addItem(pts)
-
-lns = gl.GLLinePlotItem(pos=pos, color=color, mode='lines')
-w.addItem(lns)
+test[7].setData(color=[1,0,0,1])
 
 pg.exec()
