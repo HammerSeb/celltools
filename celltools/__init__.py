4606b059ab4955e95abc64ed3298f9a3e579929b
HammerSeb <sebastian@hammer-mail.com>
2023-10-13 16:11:06 -0400

    fix

--- celltools/__init__.py
+++ celltools/__init__.py
@@ -1,3 +1,3 @@
-from cell.generate import cell_from_cif
-from cell import Cell, SuperCell, move, rotate
-from draw import make_figure, draw_cell, draw_supercell
+from .cell.generate import cell_from_cif
+from .cell import Cell, SuperCell, move, rotate
+from .draw import make_figure, draw_cell, draw_supercell
