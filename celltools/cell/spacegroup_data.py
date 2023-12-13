4fda2d30ccd8e7d3e7cacda951f0b846a0c84a11
HammerSeb <sebastian@hammer-mail.com>
2023-12-13 15:15:02 -0500

    doc string added

--- celltools/cell/spacegroup_data.py
+++ celltools/cell/spacegroup_data.py
@@ -1,18 +1,21 @@
+import typing
 import pathlib
 from os.path import join
 from re import findall
 from celltools.cell.tools import SymmetryOperator, create_SymmetryOperator
 
-def read_sym_file(file):
+def read_sym_file(file: str) -> SymmetryOperator:
     """
-
+    This function reads a symmetry file in celltools/cell/sg_symmetries and returns a SymmetryOperator. It's an
+    auxiliary function that is used in cell_from_cif if no symmetry is provided in the cif-file.
     Parameters
     ----------
-    file
+    file: str
+        symmetry file
 
     Returns
     -------
-
+        SymmetryOperator
     """
 
     path_to_sg_symmetries = join(pathlib.Path(__file__).parent.resolve(),"sg_symmetries")
@@ -25,7 +28,10 @@ def read_sym_file(file):
             )
     return sg_operator
 
+
+
 SPACE_GROUP= {
+    """ This dictionary points to all the symmetry files for the respective space group that have been implemented"""
     "1": "1.sym",
     "2": "2.sym",
     "14": "14.sym"
