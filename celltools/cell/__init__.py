2e91edd4312d740d3043d3a684148ed0533a39cd
HammerSeb <sebastian@hammer-mail.com>
2023-12-15 11:25:23 -0500

    load Bond in init

--- celltools/cell/__init__.py
+++ celltools/cell/__init__.py
@@ -1,6 +1,6 @@
 import typing
 from typing import Tuple
 
-from .contents import Atom, Molecule, Lattice, Cell
+from .contents import Atom, Bond, Molecule, Lattice, Cell
 from .tools import move, rotate, molecular_plane, SuperCell
 
