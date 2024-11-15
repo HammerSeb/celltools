51d5f52549032cfa829bbe6ccb7e13b8015491c8
HammerSeb <sebastian@hammer-mail.com>
2024-11-15 12:03:43 +0100

    git version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="celltools",
-    version="0.4.0",
+    version="0.5.0",
     description="Tools to manipulate atoms and molecules in a unit cell",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
